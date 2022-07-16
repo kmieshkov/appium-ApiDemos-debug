e2e8f04adf523734ab54ffee1f0b33f190f2949d
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2022-07-15 23:46:29 -0400

    scroll gestures added

--- src/test/java/AppiumBasics.java
+++ src/test/java/AppiumBasics.java
@@ -1,5 +1,6 @@
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
@@ -36,5 +37,38 @@ public class AppiumBasics extends BaseTest {
 		String menu = driver.findElement(AppiumBy.id("android:id/title")).getText();
 		Assert.assertEquals(menu, "Sample menu");
 		Assert.assertTrue(driver.findElement(AppiumBy.id("android:id/title")).isDisplayed());
+	}
+
+	// Scroll method used if no prior data
+	// Views > WebView
+	@Test
+	public void ScrollGestureJavaScript() throws InterruptedException {
+		driver.findElement(AppiumBy.accessibilityId("Views")).click();
+
+		// scroll till end of the page
+		boolean canScrollMore;
+		do {
+			canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+					"left", 100,
+					"top", 100,
+					"width", 200,
+					"height", 200,
+					"direction", "down",
+					"percent", 3.0
+			));
+		} while (canScrollMore);
+
+		Thread.sleep(2000);
+	}
+
+	// This scroll method used if the element known prior
+	// Views > WebView
+	@Test
+	public void ScrollGesture() {
+		driver.findElement(AppiumBy.accessibilityId("Views")).click();
+		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"))")).click();
+		driver.findElement(AppiumBy.xpath("//android.view.View[@content-desc='i am a link']/android.widget.TextView")).click();
+		String text = driver.findElement(AppiumBy.className("android.view.View")).getText();
+		Assert.assertEquals(text, "I am some other page content");
 	}
 }
