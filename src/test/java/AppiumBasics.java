d7fe1c0a8ba889c3cd77ffa42005adba4ac2c0d3
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2022-07-15 20:22:36 -0400

    appium - start programmatically

---
+++ src/test/java/AppiumBasics.java
@@ -0,0 +1,28 @@
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppiumBasics extends BaseTest {
+
+	@Test
+	public void WiFiTest() {
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+		driver.findElement(AppiumBy.accessibilityId("3. Preference dependencies")).click();
+		driver.findElement(AppiumBy.id("android:id/checkbox")).click();
+		driver.findElement(AppiumBy.xpath("(//android.widget.RelativeLayout)[2]")).click();
+		String popUpText = driver.findElement(AppiumBy.id("android:id/alertTitle")).getText();
+		Assert.assertEquals(popUpText, "WiFi settings");
+		driver.findElement(AppiumBy.id("android:id/edit")).sendKeys(popUpText);
+		driver.findElement(AppiumBy.id("android:id/button1")).click();
+	}
+}
---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,46 @@
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class BaseTest {
+
+	public AndroidDriver driver;
+	AppiumDriverLocalService service;
+
+	@BeforeClass
+	public void ConfigureAppium() throws MalformedURLException {
+		service = new AppiumServiceBuilder()
+				.withAppiumJS(new File("/Users/kmieshkov/.nvm/versions/node/v18.0.0/lib/node_modules/appium/build/lib/main.js"))
+				.withIPAddress("127.0.0.1")
+				.withArgument(() -> "--base-path", "/wd/hub/")
+				.usingPort(4723)
+				.build();
+		service.start();
+
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setDeviceName("Pixel_2_XL_API_30");
+		options.setApp("/Users/kmieshkov/Projects/IdeaProjects/appium/src/test/resources/ApiDemos-debug.apk");
+
+		URL url = new URL("http://127.0.0.1:4723/wd/hub");
+		driver = new AndroidDriver(url, options);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	}
+
+	@AfterClass
+	public void TearDown() {
+		driver.quit();
+		service.stop();
+	}
+}
